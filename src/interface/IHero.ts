4e20ae3079ce399e1c2b08dd78ee7f06d637c837
diegomated1 <diegocardwr@gmail.com>
2023-04-03 15:00:36 -0500

    interfaces

---
+++ src/interface/IHero.ts
@@ -0,0 +1,18 @@
+export default interface IHero {
+    _id: string,
+    id_hero: string,
+    name: string,
+    type: string,
+    life: number,
+    atq: {
+        base: number,
+        range: number
+    },
+    def: number,
+    dmg: {
+        base: number,
+        range: number
+    },
+    power: number,
+    last_dmg: number
+};
